8e2f69cf318aa916bdb24670ce6aefa7f2d1f6e4
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2024-11-06 22:04:46 +0100

    second commit

--- starter-code/config.js
+++ starter-code/config.js
@@ -4,14 +4,17 @@ tailwind.config = {
             colors: {
                 grayish_blue: 'hsl(217, 19%, 38%)',
                 dark_grayish_blue: 'hsl(217, 19%, 24%)',
-                for_text: '#afb1c7',
+                for_text: '#fff',
                 for_number: '#ffffff',
-                dark_navy: '#000a1c',
+                dark_navy: '#0c0e18',
                 line_color: '#4e5a73',
-                navy: '#3b4962e6'
+                light_white: '#ffffff0d',
+                h1_clr: '#d2d8f9'
             },
             fontFamily: {
-                  manrope: ["Manrope", "sans-serif"]  
+                  belle: ["Bellefair", "serif"],
+                  barlow_c: ["Barlow Condensed", "sans-serif"],
+                  barlow: [ "Barlow", "sans-serif"]
             },
             backgroundImage: {
                 body_mobile_hero: 'url("./assets/home/background-home-mobile.jpg")',
