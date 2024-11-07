de1e121407d652bf1d3c5acefb7f324e148d95c0
Mariam Oluseyi-Alli <81536650+mariamALLI@users.noreply.github.com>
2024-11-07 12:01:21 +0100

    destination mobile page

--- config.js
+++ config.js
@@ -20,6 +20,15 @@ tailwind.config = {
                 body_mobile_hero: 'url("./assets/home/background-home-mobile.jpg")',
                 body_desktop_hero: 'url("./assets/home/background-home-desktop.jpg")',
                 body_tablet_hero: 'url("./assets/home/background-home-tablet.jpg")',
+                body_mobile_des: 'url("./assets/destination/background-destination-mobile.jpg")',
+                body_desktop_des: 'url("./assets/destination/background-destination-desktop.jpg")',
+                body_tablet_des: 'url("./assets/destination/background-destination-tablet.jpg")',
+                body_mobile_crew: 'url("./assets/crew/background-crew-mobile.jpg")',
+                body_desktop_crew: 'url("./assets/crew/background-crew-desktop.jpg")',
+                body_tablet_crew: 'url("./assets/crew/background-crew-tablet.jpg")',
+                body_mobile_tech: 'url("./assets/technology/background-technology-mobile.jpg")',
+                body_desktop_tech: 'url("./assets/technology/background-technology-desktop.jpg")',
+                body_tablet_tech: 'url("./assets/technology/background-technology-tablet.jpg")'
             }
         }
     }
